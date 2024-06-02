2821323883dc22a018b2f0f86312c5b6552f006f
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-06-02 19:39:16 +0200

    tests: input validation tests

--- tests/testthat/test-gmeans.R
+++ tests/testthat/test-gmeans.R
@@ -1,3 +1,26 @@
+test_that("gmeans works", {
+  # x needs to be a matrix
+  expect_error(gmeans(NULL))
+  expect_error(gmeans(letters))
+  expect_error(gmeans(numeric()))
+  expect_error(gmeans(rnorm(5L)))
+  # k_init and k_max need to be integers
+  x <- matrix(rnorm(100, sd = 0.3), ncol = 2)
+  colnames(x) <- c("x", "y")
+  expect_error(gmeans(x, k_init = NA_integer_))
+  expect_error(gmeans(x, k_init = 1:10))
+  expect_error(gmeans(x, k_init = 1.5))
+  expect_error(gmeans(x, k_max = NA_integer_))
+  expect_error(gmeans(x, k_max = 1:10))
+  expect_error(gmeans(x, k_max = 1.5))
+  # alpha needs to be a number between 0 and 1
+  expect_error(gmeans(x, alpha = NULL))
+  expect_error(gmeans(x, alpha = NA_real_))
+  expect_error(gmeans(x, alpha = 0))
+  expect_error(gmeans(x, alpha = 1))
+  expect_error(gmeans(x, alpha = c(0.5, 0.7)))
+})
+
 test_that("ad.test works", {
   expect_error(ad.test(NULL))
   expect_error(ad.test(letters))
