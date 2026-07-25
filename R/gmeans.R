c05647318e70d40efc01b649793cded682b67fab
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-07-25 11:43:44 +0200

    docs: document the return value of compute_wss() and predict()

--- R/gmeans.R
+++ R/gmeans.R
@@ -210,6 +210,7 @@ is_null_hypothesis <- function(data, centers, level = 0.05) {
 #'   Power of the Minkowski distance. Default is `2`.
 #' @param ... (`any`)\cr
 #'   Additional arguments.
+#' @returns An `integer()` vector with one cluster index per row of `newdata`.
 #' @seealso [clue::cl_predict()] to predict on a plain [stats::kmeans()] object.
 #' @source Adapted from \CRANpkg{clue}
 #' @export
@@ -246,6 +247,8 @@ predict.gmeans <- function(
 #'   Class inheriting from `"kmeans"`.
 #' @param newdata (`matrix()`)\cr
 #'   New data to predict on.
+#' @returns A `numeric()` vector with one within-cluster sum of squares per cluster, in the
+#'   order of the rows of `object$centers`. Clusters with no assigned points contribute `0`.
 #' @export
 #' @examples
 #' km <- kmeans(mtcars, 5)
