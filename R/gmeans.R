1f7685d17d2112041a91c819a362eff5a34ca4d9
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-09-11 21:40:25 +0200

    fix: use a single sample size error in ad.test

--- R/gmeans.R
+++ R/gmeans.R
@@ -370,7 +370,7 @@ rxdist <- function(
 #' ad.test(rnorm(100, mean = 5, sd = 3))
 #' ad.test(runif(100, min = 2, max = 4))
 ad.test <- function(x) {
-  stopifnot(is.numeric(x), length(x) > 7L)
+  stopifnot(is.numeric(x))
   dname <- deparse1(substitute(x))
   x <- sort(x[!is.na(x)])
   n <- length(x)
